2274b36e3d1d68997bc0dfb491481cf963107bd4
Farzad <amirifarzad1381@gmail.com>
2025-07-07 22:33:46 +0430

    Develop DispatchDomainEventsInterceptor to Trigger Domain Event Handlers

--- Backend/Modules/Catalog/Catalog/CatalogModule.cs
+++ Backend/Modules/Catalog/Catalog/CatalogModule.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Shared.Data.Interceptors;
 
 namespace Catalog;
 public static class CatalogModule
@@ -8,13 +10,27 @@ public static class CatalogModule
     public static IServiceCollection AddCatalogModule(this IServiceCollection services,
         IConfiguration configuration)
     {
+        //Application Use Case Services
+        services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+        });
+
+        // Infrastructure Services
         var connectionString = configuration.GetConnectionString("Database");
 
-        services.AddDbContext<CatalogDbContext>(options =>
+        services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptors>();
+        services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+
+
+        services.AddDbContext<CatalogDbContext>((sp ,options) =>
         {
+            options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
             options.UseNpgsql(connectionString);
         });
+
         services.AddScoped<IDataSeeder, CatalogDataSeeder>();
+        
         return services;
     }
 
